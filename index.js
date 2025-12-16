2ff089b890748e9417a55eb65513b024fd6de239
juhaer009 <juhaerjafid1998@gmail.com>
2025-12-16 12:53:51 +0600

    created endpoint for deleting contest

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const port = process.env.PORT || 3000;
 
@@ -46,6 +46,13 @@ async function run() {
       }
       const cursor = contestsCollection.find(query);
       const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.delete("/contests/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await contestsCollection.deleteOne(query);
       res.send(result);
     });
 
